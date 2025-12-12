b0984d2e30651186278b052f2679f2ef402ef581
sktii <115221105+sktii@users.noreply.github.com>
2025-12-12 13:14:21 +0800

    Update demo path in run_learner.sh

--- examples/experiments/stack_cube_sim/run_learner.sh
+++ examples/experiments/stack_cube_sim/run_learner.sh
@@ -3,5 +3,5 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python ../../train_rlpd.py "$@" \
     --exp_name=stack_cube_sim \
     --checkpoint_path=first_run \
-    --demo_path=/path/to/your/demo_data/stack_cube_sim_20_demos_DATE.pkl \
+    --demo_path=/home/wayne/hil-serl-sim2/examples/experiments/stack_cube_sim/demo_data/stack_cube_sim_1_demos_2025-12-12_11-05-37.pkl \
     --learner \
